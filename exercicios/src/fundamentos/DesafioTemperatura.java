5f9568cfcdc682d463211bac345b0004210ba248
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-07-16 11:06:36 -0300

    adicionado comentarios para estudo posterior

--- exercicios/src/fundamentos/DesafioTemperatura.java
+++ exercicios/src/fundamentos/DesafioTemperatura.java
@@ -4,9 +4,10 @@ public class DesafioTemperatura {
 	
 	public static void main(String[] args) {
 		//DESAFIO PROPOSTO DE TRANSFORMAR VALOR EM °F PARA °C
+		//FORMULA ( F - 32 ) X 5/9 
 		
 		double valorF = 26.0;
-		final double TRANSFORM = 5.0 / 9.0; //color .0 para nao devolver um numero inteiro
+		final double TRANSFORM = 5.0 / 9.0; //colocar .0 para nao devolver um numero inteiro
 		
 		double result = ( valorF - 32 ) * TRANSFORM;
 		
