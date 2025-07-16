7e700fa815579d32e61192d9e3817242a0cb6af5
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-07-16 11:00:33 -0300

    Desafio proposto transformar temperatudo, para estudo de variaveis

---
+++ exercicios/src/fundamentos/DesafioTemperatura.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class DesafioTemperatura {
+	
+	public static void main(String[] args) {
+		//DESAFIO PROPOSTO DE TRANSFORMAR VALOR EM °F PARA °C
+		
+		double valorF = 26.0;
+		final double TRANSFORM = 5.0 / 9.0; //color .0 para nao devolver um numero inteiro
+		
+		double result = ( valorF - 32 ) * TRANSFORM;
+		
+		System.out.println(result);
+		
+		valorF = 50;
+		
+		result = (valorF - 32) * TRANSFORM;
+		
+		System.out.println(result);
+		
+	}
+}
